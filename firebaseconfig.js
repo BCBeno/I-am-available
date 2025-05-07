692b86095746c328f646f70a73c94aa32ffa993b
Benoni Circiu <m322112@alunos.ipb.pt>
2025-05-07 22:53:03 +0100

    Chat feature with firebase integration

--- firebaseconfig.js
+++ firebaseconfig.js
@@ -1,6 +1,5 @@
-import { initializeApp } from "firebase/app";
+import { initializeApp, getApps } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
-import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
   apiKey: "AIzaSyA3RpZNMigoAwN8NnGRbJ5o3YKIHarSImI",
@@ -9,10 +8,11 @@ const firebaseConfig = {
   storageBucket: "i-am-available.appspot.com",
   messagingSenderId: "1007358203617",
   appId: "1:1007358203617:web:52ff965d2d525ac95349a0",
-  measurementId: "G-YXB4YJQLR7"
+  measurementId: "G-YXB4YJQLR7",
 };
 
-const app = initializeApp(firebaseConfig);
+// Initialize Firebase only if no app has been initialized
+const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApps()[0];
 const db = getFirestore(app);
 
 export { app, db };
